21fbf2cbe5a37d35517be2865f5fb523e9465314
Tony Aldea <tonythecoder@outlook.com>
2024-01-06 08:19:09 -0500

    Forgot to send this last night.

---
+++ Learn Regular Expressions by Building a Password Generator.py
@@ -0,0 +1,5 @@
+### Look more into \W and what is special about the _
+### As a reminder, practice all() and any() some to get a good grasp
+### Find a way to practice generators and for within all()
+### Is _ in python just a local variable and why not just use a letter?
+### A reminder to go back and double check on __main__ and __name__
