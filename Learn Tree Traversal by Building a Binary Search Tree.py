886a72e8f18b1c1fb8cc0e2dd5c77d711c545eb9
Tony Aldea <tonythecoder@outlook.com>
2024-01-08 23:05:17 -0500

    First Commit

---
+++ Learn Tree Traversal by Building a Binary Search Tree.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return TreeNode(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        elif key > node.key:
+            node.right = self._insert(node.right, key)
+        return node
+
+    def search(self, key):
+        return self._search(self.root, key)
