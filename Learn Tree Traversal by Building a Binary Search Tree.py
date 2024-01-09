284ad2765abfe48b5db1bf457ada66f238706131
Tony Aldea <tonythecoder@outlook.com>
2024-01-09 07:54:54 -0500

    Partial completion

--- Learn Tree Traversal by Building a Binary Search Tree.py
+++ Learn Tree Traversal by Building a Binary Search Tree.py
@@ -23,3 +23,21 @@ class BinarySearchTree:
 
     def search(self, key):
         return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.key == key:
+            return node
+        if key < node.key:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def delete(self, key):
+        self.root = self._delete(self.root, key)
+
+    def _delete(self, node, key):
+        if node is None:
+            return node
+        if key < node.key:
+            node.left = self._delete(node.left, key)
+        elif key > node.key:
+            node.right = self._delete(node.right, key)
